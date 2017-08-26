dd946a3174fec73c9c5d5a3a910b131f6797bd42
eyana-m <eyanamallari@gmail.com>
2017-08-26 21:22:48 +0800

    added Related Features and Related Requirements in django-admin

--- base/joan/admin.py
+++ base/joan/admin.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.db.models import Count
 
 from .models import Requirement
 
@@ -18,6 +19,33 @@ from .models import Ticket
 #
 #
 # admin.site.register(Feature)
-admin.site.register(Requirement)
-admin.site.register(Feature)
+
+
+class RequirementAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super(RequirementAdmin, self).get_queryset(request)
+        return qs.annotate(feature_count=Count('feature'))
+
+    def show_feature_count(self, inst):
+        return inst.feature_count
+
+    show_feature_count.short_description = "Related Features"
+    show_feature_count.admin_order_field = "feature_count"
+    list_display = ["reqd_id", "requirement_heading", "show_feature_count"]
+
+class FeatureAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super(FeatureAdmin, self).get_queryset(request)
+        return qs.annotate(requirement_count=Count('requirement'))
+
+    def show_requirement_count(self, inst):
+        return inst.requirement_count
+
+    show_requirement_count.short_description = "Related Requirements"
+    show_requirement_count.admin_order_field = "requirement_count"
+    list_display = ["feature_text", "feature_heading","show_requirement_count"]
+
+
+admin.site.register(Requirement,RequirementAdmin)
+admin.site.register(Feature,FeatureAdmin)
 admin.site.register(Ticket)
